08dcb3118e813c3469153edccef0b48cf7e826fd
tharvey <tharveyster@gmail.com>
2021-10-23 09:57:42 -0400

    Fixed typos in SavedBooks.js file

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,7 +9,7 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook] = useMutation(REMOVE_BOOK);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
   const userData = data?.me || {};
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -21,7 +21,7 @@ const SavedBooks = () => {
     }
 
     try {
-      await removeBook({
+      await deleteBook({
         variables: {bookId: bookId},
         update: cache => {
           const data = cache.readQuery({ query: GET_ME });
