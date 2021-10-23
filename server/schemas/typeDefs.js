7e52664932a6083bc6b26e51ee01c94138978972
tharvey <tharveyster@gmail.com>
2021-10-23 08:32:01 -0400

    Added typeDefs.js file to the schemas folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput): User
+    removeBook(bookId: String!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
